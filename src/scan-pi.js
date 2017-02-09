2a7ac7af421ae2bb7ee2fcbfaef331d6d66105aa
Richie Casto <Richie Casto>
2017-02-08 20:31:34 -0800

    doing it right

--- src/scan-pi.js
+++ src/scan-pi.js
@@ -3,34 +3,23 @@ require("babel-polyfill");
 var rpio = require('rpio');
 var bluetooth = require('./bluetooth');
 
-var PIN_OUT = 8;
+var PIN_OUT = 11;
 
 // Initialize pin out to low
-rpio.open(12, rpio.OUTPUT, rpio.LOW);
-
-for (var i = 0; i < 5; i++) {
-    /* On for 1 second */
-    rpio.write(PIN_OUT, rpio.HIGH);
-    rpio.sleep(1);
-
-    /* Off for half a second (500ms) */
-    rpio.write(PIN_OUT, rpio.LOW);
-    rpio.msleep(500);
-}
+rpio.open(PIN_OUT, rpio.OUTPUT, rpio.LOW);
 
 bluetooth.scanBluetoothTimer(1, 10000, (candidates) => {
     console.log('Scan frame complete');
     candidates.forEach((candidate) => {
         console.log(`Found: ${candidate.address} - ${candidate.name}`);
     });
-    console.log();
-
     var output_voltage = rpio.LOW;
     if (candidates.length > 0) {
         output_voltage = rpio.HIGH;
     }
-    console.log('Writing voltage:', output_voltage);
     rpio.write(PIN_OUT, output_voltage);
+    console.log('Writing voltage:', output_voltage);
+    console.log();
 }, (percentComplete) => {
     console.log(`Scan frame ${percentComplete}% complete`);
 }, (error) => {
