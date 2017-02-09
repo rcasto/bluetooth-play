d2cb6e27d1e3f5a36b9cc34c5cd1e5cdd115bdcf
Richie Casto <Richie Casto>
2017-02-08 20:27:53 -0800

    debug pinout

--- src/scan-pi.js
+++ src/scan-pi.js
@@ -8,6 +8,16 @@ var PIN_OUT = 8;
 // Initialize pin out to low
 rpio.open(12, rpio.OUTPUT, rpio.LOW);
 
+for (var i = 0; i < 5; i++) {
+    /* On for 1 second */
+    rpio.write(PIN_OUT, rpio.HIGH);
+    rpio.sleep(1);
+
+    /* Off for half a second (500ms) */
+    rpio.write(PIN_OUT, rpio.LOW);
+    rpio.msleep(500);
+}
+
 bluetooth.scanBluetoothTimer(1, 10000, (candidates) => {
     console.log('Scan frame complete');
     candidates.forEach((candidate) => {
