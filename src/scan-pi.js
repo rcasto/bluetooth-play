eb7549d5a2b4f54df2a91bc6ead1352572ccec30
Richie Casto <Richie Casto>
2017-02-08 20:40:13 -0800

    only output high when target is near

--- src/scan-pi.js
+++ src/scan-pi.js
@@ -4,21 +4,21 @@ var rpio = require('rpio');
 var bluetooth = require('./bluetooth');
 
 var PIN_OUT = 11;
+var TARGET = "54:40:AD:CF:46:82";
 
 // Initialize pin out to low
 rpio.open(PIN_OUT, rpio.OUTPUT, rpio.LOW);
 
 bluetooth.scanBluetoothTimer(1, 10000, (candidates) => {
     console.log('Scan frame complete');
+    var output_voltage = rpio.LOW;
     candidates.forEach((candidate) => {
         console.log(`Found: ${candidate.address} - ${candidate.name}`);
+        if (candidate.address === TARGET) {
+            output_voltage = rpio.HIGH;
+        }
     });
-    var output_voltage = rpio.LOW;
-    if (candidates.length > 0) {
-        output_voltage = rpio.HIGH;
-    }
     rpio.write(PIN_OUT, output_voltage);
-    console.log('Writing voltage:', output_voltage);
     console.log();
 }, (percentComplete) => {
     console.log(`Scan frame ${percentComplete}% complete`);
