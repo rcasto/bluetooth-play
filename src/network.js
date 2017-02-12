fc9efa5001a985b5f0216d1278ac0c3acb8f6d51
Richie Casto <Richie Casto>
2017-02-11 22:28:47 -0800

    add network helper, scan network on timer

---
+++ src/network.js
@@ -0,0 +1,56 @@
+var nmap = require('libnmap');
+
+function getItemAddresses(item) {
+    if (!item || !item.host) {
+        return [];
+    }
+    let addresses;
+    return Array.prototype.concat.apply([], item.host.map(function (host) {
+        addresses = host.address || [];
+        return addresses.map(function (address) {
+            address = address.item || {};
+            return {
+                address: address.addr || '',
+                addressType: address.addrtype || '',
+                vendor: address.vendor || ''
+            };
+        });
+    }));
+}
+
+function getAddresses(report) {
+    report = report || {};
+    return Array.prototype.concat.apply([], Object.keys(report).map(function (item) {
+        return getItemAddresses(report[item]);
+    }));
+}
+
+function scan(options) {
+    return new Promise((resolve, reject) => {
+        nmap.scan(options, function (err, report) {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(report);
+        });
+    });
+}
+
+function scanTimer(options, scanInterval, onReport, onError) {
+    var timeoutid;
+    (function _scan() {
+        scan(options)
+            .then(onReport)
+            .then(function () {
+                timeoutid = setTimeout(_scan, scanInterval);
+            })
+            .catch(onError);
+    }());
+    return () => clearTimeout(timeoutid);
+}
+
+module.exports =  {
+    getAddresses,
+    scan,
+    scanTimer
+};
--- src/scan-network.js
+++ src/scan-network.js
@@ -1,15 +1,23 @@
-var nmap = require('node-libnmap');
-var opts = {
-    verbose: true,
-    // range: [
-    //     '192.168.0.100-120'
-    // ]
-};
+require("babel-polyfill");
 
-nmap.discover(function(err, report) {
-    if (err) throw new Error(err);
- 
-    for (var item in report) {
-        console.log(JSON.stringify(report[item]));
-    }
+// var rpio = require('rpio');
+var network = require('./network');
+
+var TARGET = 'FC:DB:B3:42:4C:18';
+var PIN_OUT = 11;
+
+network.scanTimer({
+    range: [
+        '192.168.0.100-120'
+    ]
+}, 
+10000, 
+function (report) {
+    network.getAddresses(report)
+        .forEach(function (address) {
+            console.log(address.address, '-', address.vendor);
+        });
+    console.log();
+}, function (error) {
+    console.error('Something went wrong:', error);
 });
